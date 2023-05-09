1287e7ecda8fe65323f43c46e35fc2109171f5d3
GeekNekoS <anastasiyanesterenko@icloud.com>
2023-05-09 21:41:52 +0500

    backup saving

--- locators.py
+++ locators.py
@@ -1,5 +1,8 @@
 from selenium.webdriver.common.by import By
 
 
-class LoginLocators:
-    SMTH = (By.XPATH, "//div")  # exsample
+class MainPageLocators:
+    TESTS_EXAMPLES = (By.XPATH, "//div[@class='endpoints']/ul/li")
+    RESPONSE_URL = (By.XPATH, "//span[@class='url']")
+    RESPONSE_CODE = (By.XPATH, "//span[@data-key='response-code']")
+    RESPONSE_OUTPUT = (By.XPATH, "//pre[@data-key='output-response']")
