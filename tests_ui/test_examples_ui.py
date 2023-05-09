426759c04b93afaf59d17576da54dbd0461147c6
GeekNekoS <anastasiyanesterenko@icloud.com>
2023-05-09 23:47:00 +0500

    the second part of the task has been completed

--- tests_ui/test_examples_ui.py
+++ tests_ui/test_examples_ui.py
@@ -1,6 +1,5 @@
 from Page_Object import MainPage
 import logging
-import pytest
 
 
 LOGGER = logging.getLogger(__name__)
@@ -16,16 +15,12 @@ def test_examples_ui(driver):
 
     LOGGER.info('Get params')
     for params in yandex_main_page.get_test_parameters():
-        web_elem, res_code_ui, res_content_ui, req_code_api, res_content_api = params
-
-        LOGGER.info(f'\n{res_code_ui}\n{req_code_api}\n#\n#\n#')
-        LOGGER.info(f'\n{res_content_ui}\n{res_content_api}\n#\n#\n#')
-
-        # try:
-        #     assert res_code_ui == req_code_api
-        #     assert res_content_ui == res_content_api
-        # except Exception as ex:
-        #     LOGGER.info(f'Assertion error: {ex}')
+        response_code_ui, response_output_ui, response_code_api, response_content_api = params
+        try:
+            assert response_code_ui == response_code_api
+            assert response_output_ui == response_content_api
+        except Exception as ex:
+            LOGGER.info(f'Assertion error: {ex}')
 
     # //pre[@data-key='output-response' and @hidden='true']
     # //pre[@data-key='output-response']
